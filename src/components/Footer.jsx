9f506f5c9424c8456ea3d9b96dd7263a8aee953c
Jason <c04jason@gmail.com>
2025-01-28 12:14:14 -0500

    updated footer style and use constants

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
+import { PersonalInfo } from '../utils/constants';
+
 const Footer = () => {
   return (
-    <footer className="w-full max-w-2xl border rounded-sm text-center">
-      &copy; {new Date().getFullYear()} Jason Chen
+    <footer className="w-full max-w-2xl">
+      <div className="bg-gradient-to-r from-gray-800 to-gray-900 backdrop-blur-sm rounded-t-lg p-4 text-center text-white">
+        &copy; {new Date().getFullYear()} {PersonalInfo.Name}. All rights reserved.
+      </div>
     </footer>
   );
 };
