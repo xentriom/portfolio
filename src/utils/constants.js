b150e108e5ae311302ddd170bd28dfb727fc1b6d
Jason <c04jason@gmail.com>
2025-01-28 12:13:53 -0500

    created a constants file to hold immutable data

---
+++ src/utils/constants.js
@@ -0,0 +1,12 @@
+export const PersonalInfo = Object.freeze({
+  Name: 'Jason Chen',
+  Email: 'jc5892@rit.edu',
+  Education: 'Rochester Institute of Technology',
+  Location: 'Rochester, NY',
+});
+
+export const NavItems = Object.freeze([
+  { name: 'Home', href: '/' },
+  { name: 'Projects', href: '/projects' },
+  { name: 'Contact', href: '/contact' },
+]);
