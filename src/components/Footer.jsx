49089f25796e8910e6f36b2a452f783a603908bb
Jason <c04jason@gmail.com>
2025-02-02 22:48:50 -0500

    adjusted footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = memo(function Footer() {
   return (
     <footer className="w-full max-w-2xl bg-gradient-to-r from-gray-800 to-gray-900 backdrop-blur-sm rounded-t-lg">
       <div className="p-4 text-center text-white text-xs">
-        &copy; {new Date().getFullYear()} {PersonalInfo.FullName}
+        &copy; {new Date().getFullYear()} {PersonalInfo.FullName}. All rights reserved.
       </div>
     </footer>
   );
