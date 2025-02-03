fadc3a3622909e9fbed7abeeb6612fea48c8a11f
Jason <c04jason@gmail.com>
2025-02-02 22:13:24 -0500

    added hover to all tab regardless of active

--- src/components/NavTab.jsx
+++ src/components/NavTab.jsx
@@ -5,8 +5,8 @@ const NavTab = ({ to, name }) => {
     <NavLink
       to={to}
       className={({ isActive }) =>
-        `flex-grow text-center px-3 py-2 font-medium transition duration-200 ${
-          isActive ? 'text-white border-b-2 border-blue-500' : 'text-gray-300 hover:text-white hover:bg-gray-700'
+        `flex-grow text-center px-3 py-2 font-medium transition duration-200 hover:bg-gray-700 ${
+          isActive ? 'text-white border-b-2 border-blue-500' : 'text-gray-300 hover:text-white'
         }`
       }
     >
