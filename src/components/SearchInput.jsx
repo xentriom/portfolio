809133cea614a1a9b00d3eefd96181c5817bf837
Jason <c04jason@gmail.com>
2025-02-04 22:02:17 -0500

    memoized search bar

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,19 +1,24 @@
+import { memo } from 'react';
 import { Search } from 'lucide-react';
 import MemoizedIcon from './MemoizedIcon';
 import PropTypes from 'prop-types';
 
-const SearchInput = ({ value, onChange }) => (
-  <div className="relative w-full max-w-lg">
-    <MemoizedIcon Icon={Search} className="absolute left-3 top-3 text-gray-400" />
-    <input
-      type="text"
-      placeholder="Search projects..."
-      value={value}
-      onChange={onChange}
-      className="pl-10 pr-4 py-2 w-full border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-    />
-  </div>
-);
+// SearchInput component for project searches
+const SearchInput = memo(function SearchInput({ value, onChange }) {
+  return (
+    <div className="relative w-full max-w-lg">
+      <MemoizedIcon Icon={Search} className="absolute left-3 top-3 text-gray-400" />
+      <input
+        type="text"
+        name="search"
+        placeholder="Search projects..."
+        value={value}
+        onChange={onChange}
+        className="pl-10 pr-4 py-2 w-full border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+      />
+    </div>
+  );
+});
 
 SearchInput.propTypes = {
   value: PropTypes.string.isRequired,
