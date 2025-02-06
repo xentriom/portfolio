a3af45120c12e4c056d56d5507a5a416f96a2d57
Jason <c04jason@gmail.com>
2025-02-06 13:00:26 -0500

    vercel analytics and rewrite rules

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App';
 import './utils/firebase';
 import './index.css';
@@ -9,6 +10,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </StrictMode>
 );
