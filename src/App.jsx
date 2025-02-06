831aa24d397d53691b84a2508931f962faf8f0d4
Jason <c04jason@gmail.com>
2025-02-05 22:54:24 -0500

    footer-less????

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     <div className="min-h-screen flex flex-col items-center">
       <Header />
       <NavBar />
-      <main className="w-full max-w-2xl flex flex-col flex-grow mb-4 p-6 bg-gradient-to-r from-gray-800 to-gray-900 backdrop-blur-sm text-white rounded-b-lg shadow-lg">
+      <main className="w-full max-w-2xl flex flex-col flex-grow p-6 bg-gradient-to-r from-gray-800 to-gray-900 backdrop-blur-sm text-white shadow-lg">
         <Suspense fallback={<Loading />}>
           <Routes>
             <Route path="/" element={<Home />} />
@@ -26,7 +26,7 @@ function App() {
           </Routes>
         </Suspense>
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,29 @@
+// import { memo } from 'react';
+// import { PersonalInfo } from '../utils/constants';
+
+// // Header component for displaying personal information
+// const Header = memo(function Header() {
+//   return (
+//     <header className="w-full max-w-2xl bg-gradient-to-r from-gray-800 to-gray-900 backdrop-blur-sm text-white p-6 rounded-b-lg shadow-lg mb-4 cursor-default">
+//       <div className="flex flex-row items-center justify-start ">
+//         <img src={'./assets/initial.png'} alt="J" className="w-24 h-24 shadow-lg" />
+//         <h1 className="text-3xl font-bold tracking-tight pl-[-10]">{PersonalInfo.FullName.slice(1)}</h1>
+//       </div>
+//     </header>
+//   );
+// });
+
+// export default Header;
+
 import { memo } from 'react';
-import { Mail, MapPin } from 'lucide-react';
 import { PersonalInfo } from '../utils/constants';
 
-// Header component for displaying personal information
 const Header = memo(function Header() {
   return (
-    <header className="w-full max-w-2xl bg-gradient-to-r from-gray-800 to-gray-900 backdrop-blur-sm text-white p-6 rounded-b-lg shadow-lg mb-4 cursor-default">
-      <div className="flex flex-col md:flex-row md:items-center md:justify-between">
-        <div className="space-y-2">
-          <h1 className="text-3xl font-bold tracking-tight">{PersonalInfo.FullName}</h1>
-          <p className="text-gray-300 text-sm font-medium">{PersonalInfo.Education}</p>
-        </div>
-
-        <div className="mt-4 md:mt-0 flex flex-col items-start md:items-end space-y-1">
-          <div className="flex items-center space-x-2">
-            <Mail size={16} className="text-blue-400" />
-            <span className="text-gray-300 text-sm">{PersonalInfo.Email}</span>
-          </div>
-          <div className="flex items-center space-x-2">
-            <MapPin size={16} className="text-blue-400" />
-            <span className="text-gray-300 text-sm">{PersonalInfo.Location}</span>
-          </div>
-        </div>
+    <header className="w-full max-w-2xl bg-gradient-to-r from-gray-800 to-gray-900 backdrop-blur-sm text-white py-2 rounded-b-lg shadow-lg mb-4 cursor-default">
+      <div className="flex items-center justify-center">
+        <img src={'./assets/initial.png'} alt="J" className="w-12 h:12 md:w-24 md:h-24" />
+        <h1 className="font-bold tracking-tight -ml-1 md:text-3xl md:-ml-3">{PersonalInfo.FullName.slice(1)}</h1>
       </div>
     </header>
   );
