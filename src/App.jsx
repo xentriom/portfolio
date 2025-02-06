236fe431fde969630f48b9b29de6d5c48c531ce6
Jason <c04jason@gmail.com>
2025-02-06 13:23:39 -0500

    fixed insight import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { useFirebaseTracking } from './hooks/useFirebaseTracking';
 import Header from './components/Header';
 import Loading from './components/Loading';
