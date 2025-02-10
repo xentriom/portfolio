b8f70366f2cb906dac20c27115ec421d578f7ba8
Jason <c04jason@gmail.com>
2025-02-09 23:10:02 -0500

    added better placeholder to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,8 @@ const Home = () => {
     <>
       <title>Jason Chen</title>
       <meta name="description" content="Hey, I'm Jason, a student at RIT. Welcome to my page!" />
-      <p>Home content.</p>
+      <p>Heyo~</p>
+      <p>Home page is still <b>under constructions</b>, i have something grand planned :D</p>
     </>
   );
 };
